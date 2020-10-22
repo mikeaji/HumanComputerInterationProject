cc32f86ccfcdd999064e01e505cb197227fb7212
Michael Ajibola <michaelajibola@Michaels-MacBook-Air-4.local>
2020-10-23 00:10:15 +0100

    HCI

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>                  <!-- Reg.Number 1701303-->
+<head>
+    <title>Index</title>
+    <meta charset="utf-8">
+    <meta name="viewpoint" content="=width=device-width,initial-scale=1.0">
+    <meta http-equiv="X UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="stylesheet.css" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" type="text/css">   <!-- This clearly shows the reference I used on website https://fontawesome.com/start in order to get the social media icon images  -->
+</head>
+<body>
+<!-- start of the body-->
+<header class="v-header container">
+    <div class ="fullscreen-video-wrap">
+        <!-- video clip being played-->
+        <video src="Video.MOV" autoplay="true"
+        loop="true">
+
+        </video>
+
+    </div>
+    <div class="header-overlay"
+    <!-- start of the div-->
+        <h1>Welcome Aboard !! </h1>
+        <p>Hi there! My name is Colin Ritman and I want to show you who I am!</p>
+
+        <a href="#Fire" class="btn">About me </a>
+        <a href="#Water" class="btn">Awards </a>
+        <a href="#Racks" class="btn">Projects </a>
+        <a href="#Koromah" class="btn">Research </a>
+        <a href="#Snow" class="btn">Conference </a>
+
+
+</header>
+
+
+
+<div class="container-2">
+    <!-- start of the div class-->
+    <div class="container-2-box">
+        <a name="Racks"><h3> Latest Virtual Reality Project</h3></a>
+        <p>My latest game that I created allows the user to have an immense experience on a virtual reality platform by using your Iphone to play !! The app can be download via
+                the app store only for Apple products, feel free to check my github page to check the code and on my previous projects!! Click below to purchase! </p>
+        <a href="https://www.apple.com/uk/ios/app-store/"><img src="appstore.jpg" alt="appstore" class="containme"></a> <!-- https://www.google.com/search?q=app+store&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjB5YidqMvgAhW_VRUIHZh0D10Q_AUIECgD&biw=1440&bih=748#imgrc=de8se98gaw3suM:-->
+
+    </div>
+    <div class="container-2-box">
+        <!-- start of the div-->
+        <h3>Virtual Reality</h3>
+        <p> Looks fun yes? YOU WOULD NOT BELIEVE YOUR EYES. </p>
+        <img src="virtual.jpg" alt="vituall" class="roundeded"> <!-- https://www.google.com/search?biw=1440&bih=748&tbm=isch&sa=1&ei=Rc5tXOKHLdav1fAPkfu34As&q=virtual+reality+&oq=virtual+reality+&gs_l=img.3..35i39l2j0i67l2j0l6.16750.16750..16968...0.0..0.72.72.1......1....1..gws-wiz-img.oCGFhrywXEI#imgrc=eLinQB-4gWtAKM:-->
+    </div>
+    <div class="container-2-box">
+        <!-- start of the div-->
+        <a name="Koromah"><h3>So what is Virtual Reality?</h3></a>
+        <p> Virtual Reality is a way of detecting a persons presence in a game via bio-sensing, these sensors can be attached to a data glove
+        ,glasses or even suits in order to record movements made by that person; also being in sync with the gameplay so the movements made by the person
+        wearing the equipment triggers a range of responses made by the AI. So essentially YOU become a vital part of the game thus, makes it
+        an immersive experience for example you could think you were in space yet when taking off the Virtual Reality glasses; still be in the same location
+        you are still in physically - MIND BLOWING </p>
+        <img src="reality.jpg" alt="virtually" class="thisclass">  <!-- start of the image link-->
+    </div>
+</div>
+
+<div class="container-3">
+    <div class="container-3-box">
+        <!-- start of the div-->
+        <h3>Music in the gaming industry</h3>
+        <p> Music is the confluence and a powerful tool that is used in games as it controls emotions and can establish the tone of the story being told within that game
+        this allows the gamer to really be apart of that game internally so the best user experience possible!</p>
+    </div>
+    <div class="container-3-box">
+        <!-- start of the div-->
+        <h3>Are you bored? </h3>
+        <p> CLICK BELOW TO PLAY </p>
+        <a href="https://worldsbiggestpacman.com/"><img src="pac.jpg" alt="pacman" class="pacman1"></a> <!-- https://worldsbiggestpacman.com-->
+    </div>
+    <div class="container-3-box">
+        <!-- start of the div-->
+        <h3>Previous projects!</h3>
+        <p>These include ....
+            <strong> An unbeatable Tic-Tac-toe engine - writing an engine that cannot be beaten at Tic-Tac-toe </strong>
+
+            Slack bot app - To build a social chat-room with people within a 100m radius with internet connection. <strong> Check my Github page to view the other exciting projects!</strong></p>
+    </div>
+    <div class="container-3-box">
+        <!-- start of the div-->
+        <h3>The feel</h3>
+        <p>Have you ever played a game? If not, why? Playing games allowed me to have the freedom of my mind, free to think, free to roam
+        escaping the world realities! Building dreams, accomplishing goals out of the tools in your hand!</p>
+    </div>
+    <div class="container-3-box">
+        <!-- start of the div-->
+        <h3>Copyright Information</h3>
+        <p> Colin Ritman supports compliance with the copyright law. Unauthorized copying of work, distribution and other users of copyrighted
+        material are illegal and can cause you to serve civil and criminal liability under copyright law.</p>
+    </div>
+    <div class="container-3-box">
+        <!-- start of the div-->
+        <h3>The Top 5 best games out 2019gtg4g4g!</h3>
+        <ul>
+            <li><p> Gematsu 14.17% (431 votes)</p></li>
+            <li><p> Fifa 19 12.17% (331 votes)</p></li>
+            <li><p> Red Redemption 8.17% (221 votes)</p></li>
+            <li><p> Rockafella 7.27% (131 votes)</p></li>
+            <li><p> Battleships 4.17% (31 votes)</p></li>
+
+
+
+        </ul>
+
+    </div>
+</div>
+<div class="wrapper">
+    <!-- start of the div-->
+    <h1>Check Out My Social Media Pages! Give me a follow or lets chat!</h1>
+
+<div class="media">
+    <!-- start of the div for media links-->
+
+        <a class="btn" href="https://twitter.com"><i class="fab fa-twitter"></i></a>  <!-- start of the link icons, referenced above-->
+
+        <a class="btn" href="https://www.youtube.com"><i class="fab fa-youtube"></i></a>
+
+        <a class="btn" href="https://github.com"><i class="fab fa-github"></i></a>
+
+        <a class="btn" href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
+
+        <a class="btn" href="https://www.facebook.com"><i class="fab fa-facebook"></i></a>
+</div>
+    <!-- start of the unordered list -->
+<ul>
+   <li> <a name="Fire"><img src="colin.jpg" alt="colinn" class="round"   //remember to add the width and height here.></a><li>  <!-- https://www.google.com/search?q=colin+ritman&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj1_uKfp8vgAhXPTBUIHfI7DpQQ_AUIDigB&biw=1440&bih=748#imgrc=whUvDH_W-4RoSM:-->
+        <strong><p>My name is Colin Ritman and I am a passionate, hard working games developer. After finishing University I have worked at numerous companies where I have learnt multiple skills. One thing about my self or I'd say my mind is that
+        I always think ahead, how the game could feel, what factors make the program appealing and addictive. In this page you will see current,
+        previous and future work I will endeauyhyhtyhyhhhyhytvour in, the gaming industry, technology and music as confluence is so huge its beyond comprehension;I AM
+                TALKING OUT OF THIS WORLD!. You do not want to miss what I have in store for you my friend, colleague, potential employer and the world! Lets take over the world together!</p></strong>
+
+
+    <li> <a name="Water"><img src="gameaward.jpeg" alt="award" class="roundeds"></a></li> <!-- https://www.google.com/search?q=game+awards+2018&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjtmvLKp8vgAhXvVBUIHXkxAL8Q_AUIECgD&biw=1440&bih=748-->
+    <strong><p>This shows the award that I recently won at the 2018 Gaming industry awards at London 02! You can watch the footage of my acceptance
+            speech on the youtube link above! To all people that were there, thank you for the support and votes!</p></strong>
+
+
+    <li> <a name="Snow"><img src="conference.jpg" alt="conferenceCall" class="rounded"></a></li> <!--https://www.google.com/search?biw=1440&bih=748&tbm=isch&sa=1&ei=z81tXLyAEvuE1fAP8fGKuAk&q=game+awards+2018&oq=game+awards&gs_l=img.3.0.35i39j0i67j0l8.34275.42008..42780...10.0..0.154.1279.21j1......1....1..gws-wiz-img.....0.JcYA7_1HNvg#imgrc=ra-N66HrRv7GUM: -->
+    <strong><p>This shows just a glimpse of the exciting questions and discussions I was involved in at the Rockafella Gaming Conference
+    held in Bristol! I enjoy the pressure of thinking on my feet as I believe that is when excellence and brilliance is created!
+            To all that was there, I applaud you and thank you!</p></strong>
+
+</ul>
+
+
+
+</body>
+</html>
